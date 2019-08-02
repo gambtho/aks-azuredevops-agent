13cfecbf9e24011f0b16525889797c1f7756fcfc
Thomas Gamble <thomas.gamble@microsoft.com>
2019-08-02 17:30:44 -0500

    asfda

--- scripts/config.sh
+++ scripts/config.sh
@@ -117,7 +117,7 @@ helm upgrade --tls --install --tiller-namespace=tiller-world agent ${RESOURCE_GR
     --set azp.url=${ADO_URL},azp.token=${ADO_TOKEN},azp.pool=${ADO_POOL} \
     --set image.repository=${RESOURCE_GROUP_NAME}.azurecr.io/devops-agent \
     --set ingress.hosts[0].host=${RESOURCE_GROUP_NAME}.${LOCATION}.cloudapp.azure.com \
-    --set ingress.hosts[0].paths='/' \
+    --set ingress.hosts[0].paths="['/']" \
     --set ingress.tls[0].hosts[0]=${RESOURCE_GROUP_NAME}.${LOCATION}.cloudapp.azure.com \
     --set ingress.tls[0].secretName=tls-secret
 
